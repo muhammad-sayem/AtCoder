7418515229b7eb1a9122b6383dd766676fa90ffb
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-05-31 07:31:58 +0600

    abc190_a

---
+++ A_Very_Very_Primitive_Game.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      sort(a)         sort(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(c==0){
+        if(a>b) cout <<"Takahashi" << endl;
+        else if(a<b) cout << "Aoki" << endl;
+        else cout << "Aoki" << endl;
+    }
+
+    else{
+        if(a>b) cout << "Takahashi" << endl;
+        else if(a<b) cout << "Aoki" << endl;
+        else{
+            cout << "Takahashi" << endl;
+        }
+    }
+
+
+    return 0;
+}
