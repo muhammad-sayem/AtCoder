98e714196e1c1f5a0ead19fbf3f3736953c7a0c9
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-08-20 00:38:57 +0600

    abc206_c

---
+++ C_Swappable.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;       
+
+    ll n, i;
+    cin >> n;
+    ll a[n+5];
+
+    map<int, int> mp;
+
+    for(i=0; i<n; i++){
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+
+    ll sum = 0;
+
+    // for(auto it = mp.begin(); it != mp.end(); it++){
+    //     cout << it->first << " " << it->second << endl;
+    // }
+
+    for(i=0; i<n; i++){
+        ll curr = n - i;
+        sum += curr - mp[a[i]];
+        mp[a[i]]--;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
+
+
