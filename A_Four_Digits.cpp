f9a61ed52d1ee2151eda212a0f9a393a137d6342
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-08-23 00:48:47 +0600

    abc222_a

---
+++ A_Four_Digits.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;   
+
+    string str;
+    cin >> str;
+    int i;
+
+    int sz = str.size();
+
+    if(sz == 4) cout << str << endl;
+    else{
+        int val = 4-sz;
+
+        for(i=1; i<=val; i++){
+            cout << '0';
+        }
+        cout << str << endl;
+    }  
+
+    return 0;
+}
